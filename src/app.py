ef418a0a3d98c7ce7473abd235f8536278e47adf
fernando <fernando.babonoyaba@gmail.com>
2023-12-31 23:13:25 -0700

    data collection

--- src/app.py
+++ src/app.py
@@ -1,19 +1,40 @@
-#!/usr/bin/env python3
 
-from flask import Flask, request
+#!/usr/bin/env python3
+import requests
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///Weather.sqlite3'
 
-@app.route("/")
-def main():
-    return '''
-     <form action="/echo_user_input" method="POST">
-         <input name="user_input">
-         <input type="submit" value="Submit!">
-     </form>
-     '''
+'''
+Define the database model
+that is used to store
+the temperature.
+'''
 
-@app.route("/echo_user_input", methods=["POST"])
-def echo_input():
-    input_text = request.form.get("user_input", "")
-    return "You entered: " + input_text
+db = SQLAlchemy(app)
+class Weather(db.Model):
+    datetime = db.Column(db.DateTime, primary_key=True, default=datetime.utcnow())
+    temperature = db.Column(db.Integer, nullable=False)
+    
+'''
+Helper function to get temperature
+using API
+'''
+
+def get_temperature():
+    response = requests.get("https://weatherdbi.herokuapp.com/data/weather/boulder")
+    return response.json()["currentConditions"]["temp"]["c"]
+
+
+'''
+In main we first get the current temperature and then
+create a new object that we can add to the database.
+'''
+if __name__ == "__main__":
+    current_temperature = get_temperature()
+    new_entry = Weather(temperature=current_temperature)
+    db.session.add(new_entry)
+    db.session.commit()
